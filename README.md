2a58e9b2c2784724d015db12219a80db44e59d39
Wei Kang <chongweikang5@gmail.com>
2023-02-22 20:37:38 +0800

    edit main.py

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
 To use:
 
 ```
-1. Get Telegram Bot ID.
-2. pip install -r requirements.txt
-3. Run Python Script.
+1. Create Telegram Bot & Telegram Group.
+2. Get Telegram Bot API Key.
+3. Replace it in main.py
+4. Install required dependencies
+5. Run Python Script.
+```
+
+Key Features:
+```
+1. Send quote to you upon demand.
+2. Send customised quote to you upon demand.
+3. Add customised quote of your preference.
+4. Delete any customised quote you don't want anymore.
 ```
 
 Learning points:
-
-- How to set up Telegram bot with python-telegram-bot version 13.7
-- How to customise Telegram commands
+```
+- How to set up Telegram bot with python-telegram-bot version 13.7 
+- How to customise Telegram commands with CommandHandler
 - How to use env files to set up environment variables using dotenv library (pip install python-dotenv)
+- Holding conversations with others using ConversationHandler
 - Getting Quotes from 'https://zenquotes.io/api/random' by sending requests
 - How to generate requirements.txt programatically (pip freeze > requirements.txt)
 - Using repl.it to host and using uptime robot to ping the website to ensure the website is hosted on the server permanently.
+```
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import requests
-import random
-import time
 import os
-from dotenv import load_dotenv
+import random
+import requests
+from telegram.ext.conversationhandler import ConversationHandler
 from telegram.ext.updater import Updater
 from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
@@ -10,49 +9,23 @@ from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
 
+WEIKANGBOT_ID = os.environ['WEIKANGBOT_ID']
+WEI_TESTBOT_ID = os.environ['WEI_TESTBOT_ID']
 
-load_dotenv()
-
-JREMPIRE_ID = os.getenv('JREMPIRE_ID')
-WEIKANGBOT_ID = os.getenv('WEIKANGBOT_ID')
-WEI_TESTBOT_ID = os.getenv('WEI_TESTBOT_ID')
-
-messages = [
-    'Haii',
-    'How is Rui and Ruth?',
-    'How is Gwen?',
-    'How is Ervin?',
-    'May we gather our focus?',
-    'If Ervin has no supporters, then that means I am no longer on earth. If the world is against Ervin, then I am against the world.',
-    'Ervin King',
-    'Ervin Qing',
-    'SES',
-    'Dat Vid',
-    'Ervin Feii',
-    'Wat',
-    'I AM Bot',
-    'KONAN Is Sub-Man',
-    'Konan Head is Quite Puny',
-    'Rui is Man',
-    'You may bring a COW...',
-    'You may bring a HORSE...',
-    'Rui is Man',
-    'jaixer is Golden Man',
-    'Rui is Half-Kane',
-    'Kane is Superior',
-    'FXXK Glenn',
-    "Rui is a man of many talents, that's for sure!",
-    'Yes, Rui is a man. What else can I help you with?',
-    'Interesting. Tell me more about why you want to bring a cow or a horse.'
-]
 
 def main():
 
+    # STATES
+    ADDING_QUOTE, DELETING_QUOTE = 0,0
+
+    # UPDATER
     updater = Updater(WEIKANGBOT_ID,
                     use_context=True)
-    async def getQuote():
+
+    # DAILY QUOTES
+    def getQuote():
         try:
-            resp = await requests.get('https://zenquotes.io/api/random')
+            resp = requests.get('https://zenquotes.io/api/random')
         except:
             return "Failed to get quote! Try again later!"
         data = resp.json()
@@ -61,29 +34,98 @@ def main():
         finalised_quote = (f"{quote} - {author}")
         return finalised_quote
 
+    # START BOT 
     def start(update: Update, context: CallbackContext):
         update.message.reply_text(
             "Hi, I'm Wei Bot. Write /help to see the commands available.")
 
+    # HELLO FUNCTION
     def hello(update: Update, context: CallbackContext):
         update.message.reply_text('Hello there Nerd.')
 
+    # SEND A RANDOM MESSAGE.
     def message(update: Update, context: CallbackContext):
-        random_no = random.randint(0,len(messages)-1)
-        text = messages[random_no]
+        random_no = random.randint(0,len(db['messages'])-1)
+        text = db['messages'][random_no]
         update.message.reply_text(text)
-    
-    async def quote(update: Update, context: CallbackContext):
-        quote = await getQuote()
+
+    # GET THE LIST OF MESSAGES
+    def getMessages(update: Update, context: CallbackContext):
+        messagesList = ''
+        for i in range(len(db['messages'])):
+          messagesList = messagesList + str(i+1) + '. '
+          messagesList += db['messages'][i]
+          messagesList += '\n'
+        update.message.reply_text(messagesList)
+      
+    # INTRO STATE FOR ADDING CONVERSATION HANDLER
+    def introAddMessages(update: Update, context: CallbackContext):
+        update.message.reply_text('Key in a quote you want to add.')
+        return ADDING_QUOTE
+
+    # ADDING STATE FOR ADDING CONVERSATION HANDLER
+    def addUserQuote(update:Update,context:CallbackContext):
+      userQuote = update.message.text
+      returnString = f"Saving {userQuote}...."
+      db['messages'].append(userQuote)
+      update.message.reply_text(returnString)
+      update.message.reply_text("Saved Quote.")
+      # return ConversationHandler.END to end the conversation
+      return ConversationHandler.END
+
+    # INTRO STATE FOR DELETING CONVERSATION HANDLER
+    def introDeleteMessages(update: Update, context: CallbackContext):
+        update.message.reply_text('Key in the number of the quote you want to delete.')
+        return DELETING_QUOTE
+
+    # DELETING STATE FOR DELETING CONVERSATION HANDLER
+    def deleteUserQuote(update:Update,context:CallbackContext):
+
+      # CONVERT TO INT & ERROR HANDLING.
+      try:
+        userChoice = int(update.message.text)
+      except ValueError:
+        text = f"Enter the number of the quote you want to delete only. No playing punk. (e.g. 1 to {len(db['messages'])}) "
+        update.message.reply_text(text)
+        update.message.reply_text("Let you try again.")
+        return DELETING_QUOTE
+
+      # ERROR HANDLING
+      if userChoice < 1 or userChoice > len(db['messages']):
+        update.message.reply_text("Dun play punk. No such quote.")
+        update.message.reply_text("Let you try again.")
+        return DELETING_QUOTE
+      
+      returnString = f"Deleting '{db['messages'][userChoice-1]}'...."
+      update.message.reply_text(returnString)
+      del db['messages'][userChoice-1]
+      update.message.reply_text("Deleted Quote.")
+      # return ConversationHandler.END to end the conversation
+      return ConversationHandler.END
+      
+    # QUIT STATE FOR CONVERSATION HANDLERS
+    def quit(update:Update,context:CallbackContext):
+      update.message.reply_text('Saved Quote.')
+      return ConversationHandler.END
+
+    # GET REAL INSPIRATIONAL QUOTES
+    def quote(update: Update, context: CallbackContext):
+        quote = getQuote()
         update.message.reply_text(quote)
 
+    # HELP FUNCTION
     def help(update: Update, context: CallbackContext):
-        update.message.reply_text("""Available Commands :-
+        update.message.reply_text("""Available Commands :
         /start - Start Wei
         /hello - Say Hi to Wei
         /linkedin - Get Wei's LinkedIn URL.
         /youtube - Get the link to Youtube
-        /message - Get your well-customised message from Wei """)
+        /quote - Get your daily quote from Wei
+        /message - Get your well-customised message from Wei
+        /getmessages - Get the full list of customised messages from Wei
+        /addmessages - Add your customised message to Wei
+        /deletemessages - Delete a message of your choice from Wei
+        """)
 
     def youtube_url(update: Update, context: CallbackContext):
         update.message.reply_text("Youtube Link => https://www.youtube.com/watch?v=dQw4w9WgXcQ")
@@ -99,34 +141,40 @@ def main():
     #     resp = ai.message(update.message.text)
     #     update.message.reply_text(str(resp['intents'])[0]['name'])
 
+    # HANDLE UNKNOWN COMMANDS
     def unknown(update: Update, context: CallbackContext):
         update.message.reply_text(
             "Sorry '%s' is not a valid command" % update.message.text)
 
+    # HANDLERS FOR CONVERSATIONS
+    updater.dispatcher.add_handler(ConversationHandler(
+        entry_points=[CommandHandler('addmessages', introAddMessages)],
+        states={
+            ADDING_QUOTE: [MessageHandler(Filters.text, callback=addUserQuote)]
+        },
+        fallbacks=[CommandHandler('quit', quit)]
+    ))
+    updater.dispatcher.add_handler(ConversationHandler(
+        entry_points=[CommandHandler('deletemessages', introDeleteMessages)],
+        states={
+            DELETING_QUOTE: [MessageHandler(Filters.text, callback=deleteUserQuote)]
+        },
+        fallbacks=[CommandHandler('quit', quit)]
+    ))
 
-    def unknown_text(update: Update, context: CallbackContext):
-        update.message.reply_text(
-            "Sorry I can't recognize you , you said '%s'" % update.message.text)
-
-
+    # HANDLERS FOR COMMANDS
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CommandHandler('hello', hello))
     updater.dispatcher.add_handler(CommandHandler('quote', quote))
     updater.dispatcher.add_handler(CommandHandler('message', message))
+    updater.dispatcher.add_handler(CommandHandler('getmessages', getMessages))
     updater.dispatcher.add_handler(CommandHandler('youtube', youtube_url))
     updater.dispatcher.add_handler(CommandHandler('help', help))
     updater.dispatcher.add_handler(CommandHandler('linkedin', linkedIn_url))
-    # updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
     updater.dispatcher.add_handler(MessageHandler(
         Filters.command, unknown)) # Filters out unknown commands
 
-    # Filters out unknown messages.
-    # updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
-
-    #registering Message Handler to reply to user
-    # updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command,userText))
 
     updater.start_polling()
-
 
 main()
